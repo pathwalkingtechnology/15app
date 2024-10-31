6adcbe2d2b81fb29f2d753acaf7c3b3661e08a7b
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-31 17:47:05 -0300

    V 3.0.5

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
-"use client";
-import { Routes, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import EventDetails from '../components/EventDetails';
 import ConfirmAttendance from '../components/ConfirmAttendance';
 import Map from '../components/Map';
-import GuestList from '../components/GuestList';
 
 export default function HomePage() {
   return (
     <main className="bg-gray-100 min-h-screen p-4">
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Header />
-            <EventDetails />
-            <ConfirmAttendance />
-            <Map />
-          </>
-        } />
-        <Route path="/listado" element={<GuestList />} />
-      </Routes>
+      <Header />
+      <EventDetails />
+      <ConfirmAttendance />
+      <Map />
     </main>
   );
 }
