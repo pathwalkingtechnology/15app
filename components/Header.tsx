54b47eb30cb392a9ac1646b5a8a16af185cc0b73
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-31 10:34:33 -0300

    V 1.0.1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <header className="text-center p-6">
-      <p className="italic text-lg text-gray-700">&quot;Existen momentos en la vida que imaginamos, soñamos y esperamos, uno de esos momentos ha llegado.&quot;</p>
-      <h1 className="text-4xl font-bold mt-4">Mis 15 Años</h1>
+      <p className="text-4xl mb-4 font-cursive text-gray-700">&quot;Existen momentos en la vida que imaginamos, soñamos y esperamos, uno de esos momentos ha llegado.&quot;</p>
+      <h1 className="text-6xl mb-4 font-cursive text-titleGreen">Mis 15 Años</h1>
       <h2 className="text-2xl mt-2">Joselyn Natali Valencia</h2>
     </header>
   );
