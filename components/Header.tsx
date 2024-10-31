b745c9dcb3b27e4d1fce14a8702b5c117695e429
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-31 16:32:29 -0300

    V 2.0.7

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,16 +13,24 @@ export default function Header() {
           style={{ backgroundSize: "100% auto" }}
         ></div>
         <div className="bg-transparent py-8 px-4 sm:px-8 md:px-16 lg:px-24 mx-4">
-          <p className="text-3xl sm:text-4xl font-cursive text-titleGreen italic px-4 mt-4">
+          <p className="text-2xl sm:text-3xl font-cursive text-titleGreen italic px-4 mt-4 leading-relaxed">
             &ldquo;Existen momentos en la vida que imaginamos, soñamos y esperamos,
             uno de esos momentos ha llegado.&ldquo;
           </p>
         </div>
+        <div
+          className="bg-[url('/cuerpo_pie.png')] bg-cover bg-center bg-no-repeat h-40 flex items-center justify-center"
+          style={{ backgroundSize: "100% auto" }}
+        ></div>
       </section>
 
       {/* Sección 2: Imagen de la Princesa y Detalles con Encabezado y Pie */}
       <section ref={ref}>
-         <div className="bg-transparent py-8 px-4 sm:px-8 md:px-16 lg:px-24 mx-4">
+        <div
+          className="bg-[url('/encabezado.png')] bg-cover bg-center bg-no-repeat h-40 flex items-center justify-center"
+          style={{ backgroundSize: "100% auto" }}
+        ></div>
+        <div className="bg-transparent py-8 px-4 sm:px-8 md:px-16 lg:px-24 mx-4">
           <div className="mb-6 mt-4">
             <img
               src="/princesa.png"
@@ -30,10 +38,10 @@ export default function Header() {
               className="w-full max-w-xs mx-auto"
             />
           </div>
-          <h1 className="text-5xl sm:text-6xl font-cursive text-titleGreen mb-4">
+          <h1 className="text-4xl sm:text-5xl font-cursive text-titleGreen mb-4">
             Mis 15 Años
           </h1>
-          <h2 className="text-xl sm:text-2xl text-primaryGreen mt-2">
+          <h2 className="text-lg sm:text-xl text-primaryGreen mt-2">
             Joselyn Natali Valencia
           </h2>
         </div>
