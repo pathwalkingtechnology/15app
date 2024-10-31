95c87b66b19d368b177cb93744c1560846f344bc
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-31 16:49:10 -0300

    V 2.0.9

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,10 +12,12 @@ export default function Header() {
           className="bg-[url('/encabezado.png')] bg-cover bg-center bg-no-repeat h-60 flex items-center justify-center"
           style={{ backgroundSize: "100% auto" }}
         >
-          <p className="text-4xl sm:text-5xl font-cursive text-titleGreen italic px-4 leading-relaxed">
-            &ldquo;Existen momentos en la vida que imaginamos, soñamos y esperamos,
-            uno de esos momentos ha llegado.&ldquo;
-          </p>
+          <div className="mt-20">
+            <p className="text-4xl sm:text-5xl font-cursive text-titleGreen italic px-4 leading-relaxed">
+              &ldquo;Existen momentos en la vida que imaginamos, soñamos y esperamos,
+              uno de esos momentos ha llegado.&ldquo;
+            </p>
+          </div>
         </div>
         <div
           className="bg-[url('/cuerpo_pie.png')] bg-cover bg-center bg-no-repeat h-60 flex items-center justify-center"
@@ -29,19 +31,21 @@ export default function Header() {
           className="bg-[url('/encabezado.png')] bg-cover bg-center bg-no-repeat h-60 flex items-center justify-center"
           style={{ backgroundSize: "100% auto" }}
         >
-          <div className="mb-6 mt-4">
-            <img
-              src="/princesa.png"
-              alt="Imagen de la princesa"
-              className="w-full max-w-xs mx-auto"
-            />
+          <div className="mt-20">
+            <div className="mb-6 mt-4">
+              <img
+                src="/princesa.png"
+                alt="Imagen de la princesa"
+                className="w-full max-w-xs mx-auto"
+              />
+            </div>
+            <h1 className="text-5xl sm:text-6xl font-cursive text-titleGreen mb-4">
+              Mis 15 Años
+            </h1>
+            <h2 className="text-xl sm:text-2xl text-primaryGreen mt-2">
+              Joselyn Natali Valencia
+            </h2>
           </div>
-          <h1 className="text-5xl sm:text-6xl font-cursive text-titleGreen mb-4">
-            Mis 15 Años
-          </h1>
-          <h2 className="text-xl sm:text-2xl text-primaryGreen mt-2">
-            Joselyn Natali Valencia
-          </h2>
         </div>
         <div
           className="bg-[url('/cuerpo_pie.png')] bg-cover bg-center bg-no-repeat h-60 flex items-center justify-center"
