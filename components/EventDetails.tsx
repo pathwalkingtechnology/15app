d5493181d78c189a2094d1742857905b4f1e3e8c
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-31 10:41:31 -0300

    V 1.0.2

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -1,7 +1,7 @@
 export default function EventDetails() {
   return (
     <section className="text-center p-6">
-      <p className="text-5xl mb-4 font-cursive text-gray-700">&quot;Con la bendición de Dios y mis padres, me siento feliz de llegar a este momento de mi vida, el día que dejaré atrás mi infancia y comenzaré un nuevo viaje.&quot;</p>
+      <p className="text-gray-700 italic">&quot;Con la bendición de Dios y mis padres, me siento feliz de llegar a este momento de mi vida, el día que dejaré atrás mi infancia y comenzaré un nuevo viaje.&quot;</p>
       <p className="mt-4">Sus papás Efraín Daniel Valencia y Melisa Solange Cabana te invitan a compartir este importante acontecimiento...</p>
       <div className="mt-6">
         <h3 className="text-2xl font-semibold">SÁBADO 30 NOV.</h3>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="text-center p-6">
-      <p className="text-4xl mb-4 font-cursive text-gray-700">&quot;Existen momentos en la vida que imaginamos, soñamos y esperamos, uno de esos momentos ha llegado.&quot;</p>
+      <p className="text-4xl mb-4 font-cursive text-titleGreen">&quot;Existen momentos en la vida que imaginamos, soñamos y esperamos, uno de esos momentos ha llegado.&quot;</p>
       <h1 className="text-6xl mb-4 font-cursive text-titleGreen">Mis 15 Años</h1>
       <h2 className="text-2xl mt-2">Joselyn Natali Valencia</h2>
     </header>
